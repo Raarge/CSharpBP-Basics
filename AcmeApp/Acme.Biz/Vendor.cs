148b5b485eb34eb6a6210376d391ac0d11385d91
William Darin Campbell <animusdrogai@gmail.com>
2019-01-13 23:42:59 -0500

    C# BP ImpBsc 8 through Property or Method? Added new testing with methods returning multiple results in a class.

--- AcmeApp/Acme.Biz/Vendor.cs
+++ AcmeApp/Acme.Biz/Vendor.cs
@@ -1,4 +1,5 @@
-﻿using Acme.Common;
+﻿using System;
+using Acme.Common;
 
 namespace Acme.Biz
 {
@@ -11,6 +12,37 @@ namespace Acme.Biz
         public string CompanyName { get; set; }
         public string Email { get; set; }
 
+
+        ///<summary>
+        /// Sends a product order to the vendor.
+        /// </summary>
+        /// <param name="product">Product of the order.</param>
+        /// <param name="quantity">Quantity of the product to order</param>
+        /// <returns></returns>
+        public OperationResult PlaceOrder(Product product, int quantity)   // to return multiple changes, changed this from a bool method to a OperationResult method
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+
+            var success = false;
+
+            var orderText = "Order from Acme, Inc" + System.Environment.NewLine +
+                            "Product: " + product.ProductCode + System.Environment.NewLine +
+                            "Quantity: " + quantity;
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText, this.Email);
+
+            if (confirmation.StartsWith("Message sent:"))
+            {
+                success = true;
+            }
+
+            var operationResult = new OperationResult(success, orderText);
+            return operationResult;
+        }
         /// <summary>
         /// Sends an email to welcome a new vendor.
         /// </summary>
